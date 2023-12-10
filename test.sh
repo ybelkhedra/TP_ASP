bd3c1faa148af4f33926b5f91326be590b7175e7
ybelkhedra <yamine.belkhedra@enseirb-matmeca.fr>
2023-12-10 13:50:09 +0100

    few spell fix2

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 echo "En mettant r=1, o=5, z=2 et n=3, on doit obtenir qu'un seul objet livré. En effet, le premier objet sera livré à l'étape 2, puis il faut au moins deux autres étapes pour y revenir."
 clingo logistique_V1.lp -c r=1 -c o=5 -c z=2 -c n=3 -V0 | tail -n 3 | head -n 2
 
-echo "En rajoutant seulement une étape, on est sencé pouvoir avoir deux objets livrés."
+echo "En rajoutant seulement une étape, on est censé pouvoir avoir deux objets livrés."
 clingo logistique_V1.lp -c r=1 -c o=5 -c z=2 -c n=4 -V0 | tail -n 3 | head -n 2
 
 
